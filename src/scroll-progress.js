90fdfce07d7088b3a61fe872ab11fbd2cc02055a
Johnny Hu <heyitsjhu@gmail.com>
2018-07-01 18:49:43 -0700

    Move private methods into constructor so they're not exposed

--- src/scroll-progress.js
+++ src/scroll-progress.js
@@ -8,102 +8,74 @@ export class ScrollProgress {
     console.log('//scroll-progress v0.0.1');
     this.selector = selector;
     this.options = options;
-    this.element = this.queryElement();
+    this.element = queryElement(this.selector);
     this.elementHeight = this.element.offsetHeight;
     this.elementWidth = this.element.offsetWidth;
-    this.progressBar = this.createProgressBar();
-    this.progress = this.updateProgress();
+    this.progressBar = createProgressBar();
+    this.progress = updateProgress(this);
     this.windowHeight = window.innerHeight;
     this.windowWidth = window.innerWidth;
 
-    this.initialize();
-  }
-  initialize() {
-    this.applyCSS(this.progressBar, containerStyles);
-    this.applyCSS(this.progressBar.firstChild, fillStyles);
-
-    document.addEventListener('scroll', () => {
-      this.updateProgress();
-    });
-    document.addEventListener('resize', () => {
-      this.updateProgress();
-    });
-  }
-
-  updateProgress() {
-    //--TODO: option for height vs width (vertical vs horizontal)
-    this.progress = (window.scrollY / (this.elementHeight - this.windowHeight)) * 100;
-    this.progressBar.firstChild.style.width = this.progress + '%';
-    console.log(this.elementHeight, window.innerHeight, this.windowHeight, window.scrollY, this.progress);
-  }
-  validateParameters() {
-    if (this.selector) {
-      throw new ReferenceError();
-    }
-    if (typeof selector !== 'string') {
-      throw new TypeError();
-    }
-    if (options && typeof options !== 'object') {
-      throw new TypeError();
-    }
-  }
-  applyCSS(element, styles) {
-    for (let style of styles) {
-      console.log(style);
-      element.style[style.property] = style.value;
-    }
-  }
-  applyCSSStyles() {
-    for (let option in this.options) {
-      const acceptedProperties = ['color', 'position'];
-      if (acceptedProperties.includes(option)) {
-        const fillProperties = ['color'];
-        if (fillProperties.includes(option)) {
-          this.setCSS(this.progressBar.firstChild, option);
-        } else {
-          this.setCSS(this.progressBar, option);
-        }
+    function applyCSS(element, styles) {
+      for (let style of styles) {
+        element.style[style.property] = style.value;
       }
     }
-  }
-  setCSS(element, option) {
-    let property;
-    switch (option) {
-      case 'color':
-        property = 'backgroundColor';
-        break;
-      case 'position':
-        console.log('position', option);
-        property = option;
-        element.style[this.options[option]] = 0;
-        return;
-      default:
-        break;
-    }
-    element.style[property] = this.options[option];
-  }
 
-  createProgressBar() {
-    const progressBar = this.createProgressBarContainer();
-    document.body.appendChild(progressBar);
-    return progressBar;
-  }
-  createProgressBarContainer() {
-    const container = document.createElement('div');
-    container.appendChild(this.createProgressBarFill());
-    container.classList.add('scroll-progress__container');
-    return container;
-  }
-  createProgressBarFill() {
-    const el = document.createElement('div');
-    el.classList.add('scroll-progress__fill');
-    return el;
-  }
-  queryElement() {
-    if (!document.querySelector(this.selector)) {
-      throw new ReferenceError()
+    function createProgressBar() {
+      const progressBar = createProgressBarContainer();
+      document.body.appendChild(progressBar);
+      return progressBar;
     }
-    return document.querySelector(this.selector)
-  }
 
+    function createProgressBarContainer() {
+      const el = document.createElement('div');
+      el.appendChild(createProgressBarFill());
+      el.classList.add('scroll-progress__container');
+      return el;
+    }
+
+    function createProgressBarFill() {
+      const el = document.createElement('div');
+      el.classList.add('scroll-progress__fill');
+      return el;
+    }
+
+    function queryElement(selector) {
+      if (!document.querySelector(selector)) {
+        throw new ReferenceError()
+      }
+      return document.querySelector(selector)
+    }
+
+    function updateProgress(object) {
+      //--TODO: option for height vs width (vertical vs horizontal)
+      object.progress = (window.scrollY / (object.elementHeight - object.windowHeight)) * 100;
+      object.progressBar.firstChild.style.width = object.progress + '%';
+      console.log(object.elementHeight, window.innerHeight, object.windowHeight, window.scrollY, object.progress);
+    }
+
+    function validateParameters() {
+      if (this.selector) {
+        throw new ReferenceError();
+      }
+      if (typeof selector !== 'string') {
+        throw new TypeError();
+      }
+      if (options && typeof options !== 'object') {
+        throw new TypeError();
+      }
+    }
+
+    (function initialize(object) {
+      applyCSS(object.progressBar, containerStyles);
+      applyCSS(object.progressBar.firstChild, fillStyles);
+      document.addEventListener('scroll', () => {
+        updateProgress(object);
+      });
+      document.addEventListener('resize', () => {
+        updateProgress(object);
+      });
+    })(this);
+  }
 }
