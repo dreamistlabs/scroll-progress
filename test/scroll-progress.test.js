16722ea7d2cc36812d9f6363775c88106d8b1d88
Johnny Hu <heyitsjhu@gmail.com>
2018-07-04 22:30:25 -0700

    Write more tests for default instance of ScrollProgress

--- test/scroll-progress.test.js
+++ test/scroll-progress.test.js
@@ -1,38 +1,115 @@
-var assert = chai.assert;
+const assert = chai.assert;
 
-describe('new ScrollProgress', () => {
-  it('returns a new instance of ScrollProgress', () => {
-    var scrollProgress = new ScrollProgress('.some-container');
-    assert.instanceOf(
-      scrollProgress,
-      ScrollProgress,
-      'scrollProgress should be an instance of ScrollProgress'
-    );
-  });
-  describe('the selector argument', () => {
-    it("throws a ReferenceError when it isn't provided", () => {
+const defaultContainerClassName = 'scroll-progress__container';
+const defaultInnerChildClassName = 'scroll-progress__fill';
+const defaultCSSHeight = '6px';
+const defaultCSSPosition = 'fixed';
+const defaultCSSTop = '0px';
+const defaultCSSWidth = '100%';
+
+const getElement = query => {
+  return document.getElementsByClassName(query)[0];
+};
+
+describe('the ScrollProgress instance', () => {
+  describe('when instantiated', () => {
+    it('should return a new instance of ScrollProgress', () => {
+      var scrollProgress = new ScrollProgress('.some-container');
+      assert.instanceOf(
+        scrollProgress,
+        ScrollProgress,
+        'scrollProgress should be an instance of ScrollProgress'
+      );
+    });
+    it('should create a new DOM element', () => {
+      var scrollProgress = new ScrollProgress('.some-container');
+      var element = getElement(defaultContainerClassName);
+      assert.exists(element);
+    });
+
+    it("should throw a ReferenceError if the selector argument doesn't exist", () => {
       assert.throws(function() {
         new ScrollProgress();
       }, ReferenceError);
     });
-    it("throws a TypeError when it isn't a string", () => {
+    it("should throw a TypeError when the selector argument isn't a string", () => {
       assert.throws(function() {
         new ScrollProgress({ not: 'a string' });
       }, TypeError);
     });
-  });
-  describe('the options argument', () => {
-    it("throws a TypeError when it isn't an object", () => {
-      assert.throws(function() {
-        new ScrollProgress('.some-container', 'but not an object');
-      }, TypeError);
-    });
-    it("returns undefined if one isn't declared", () => {
+    it("should assign undefined to the options argument if one isn't provided", () => {
       var scrollProgress = new ScrollProgress('.some-container');
       assert.isUndefined(
         scrollProgress.options,
         `should be undefined, but is instead ${scrollProgress.options}`
       );
     });
+    it("should throw a TypeError when the options argument isn't an object", () => {
+      assert.throws(function() {
+        new ScrollProgress('.some-container', 'but not an object');
+      }, TypeError);
+    });
+  });
+  afterEach(function() {
+    if (getElement(defaultContainerClassName)) {
+      getElement(defaultContainerClassName).remove();
+    }
+  });
+});
+
+describe('the new DOM element created by a ScrollProgress instance', () => {
+  before(function() {
+    new ScrollProgress('.some-container');
+  });
+  it('should have a CSS class name of ' + defaultContainerClassName, () => {
+    var containsClassName = getElement(
+      defaultContainerClassName
+    ).classList.contains(defaultContainerClassName);
+    assert.isTrue(containsClassName);
+  });
+  it(
+    'should contain a child element with CSS class ' +
+      defaultInnerChildClassName,
+    () => {
+      var containsElementWithCorrectClassName = getElement(
+        defaultContainerClassName
+      ).firstChild.classList.contains(defaultInnerChildClassName);
+      assert.isTrue(containsElementWithCorrectClassName);
+    }
+  );
+  it(`should have default CSS position: ${defaultCSSPosition}`, () => {
+    var elementPosition = getElement(defaultContainerClassName).style.position;
+    assert.equal(
+      elementPosition,
+      `${defaultCSSPosition}`,
+      `element's default CSS position should be ${defaultCSSPosition}`
+    );
+  });
+  it(`should have default CSS top: ${defaultCSSTop}`, () => {
+    var elementPosition = getElement(defaultContainerClassName).style.top;
+    assert.equal(
+      elementPosition,
+      `${defaultCSSTop}`,
+      `element's default CSS top property should be set to ${defaultCSSTop}`
+    );
+  });
+  it(`should have default CSS width: ${defaultCSSWidth}`, () => {
+    var elementPosition = getElement(defaultContainerClassName).style.width;
+    assert.equal(
+      elementPosition,
+      `${defaultCSSWidth}`,
+      `element's default CSS width property should be set to ${defaultCSSWidth}`
+    );
+  });
+  it(`should have default CSS height: ${defaultCSSHeight}`, () => {
+    var elementPosition = getElement(defaultContainerClassName).style.height;
+    assert.equal(
+      elementPosition,
+      defaultCSSHeight,
+      `element's default CSS width property should be set to ${defaultCSSHeight}`
+    );
+  });
+  after(function() {
+    getElement(defaultContainerClassName).remove();
   });
 });
